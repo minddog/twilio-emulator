6c3f606f93f83a62ec0896ebcccaac2f5cdc87c1
Adam Ballai <adam@blackacid.org>
2009-01-24 14:35:33 -0700

    Add gather test

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 TWILIO_SRC="/home/minddog/twilio-emulator/"
 TWML_EMU="$TWILIO_SRC/twilio-emulator.py"
 
+TESTS="multi_number.xml gather.xml"
+
 echo "Running tests..."
-$TWML_EMU "file://$TWILIO_SRC/tests/multi_number.xml"
+for test in $TESTS; do
+    $TWML_EMU "file://$TWILIO_SRC/tests/$test";
+done;
