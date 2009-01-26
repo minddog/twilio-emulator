c426b7da453f92510db16af67aa8ad9713a45b73
Adam Ballai <minddog@lebbene-globalalerts-int.local>
2009-01-26 14:23:52 -0700

    Add logging support, supports verbosity levels, can write logs to file

--- twilio-emulator.py
+++ twilio-emulator.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
 import sys, signal, os
-import urllib2
+import urllib, urllib2
 import urlparse
 import readline
+from StringIO import StringIO
 from threading import Timer
 from xml.dom.minidom import parseString
 from xml.parsers.expat import ExpatError
+from optparse import OptionParser
+from datetime import datetime
 
 class TwiMLSyntaxError(Exception):
     def __init__(self, lineno, col, doc):
@@ -19,37 +22,85 @@ class TwiMLSyntaxError(Exception):
         return "TwiMLSyntaxError at line %i col %i near %s" \
             % (self.lineno, self.col, self.line)
 
+class ResponseLogger(object):
+    def __init__(self, level=0, verbose=False, filename=None):
+        if not filename:
+            filename = "%s-twilio-log" % datetime.now()
+        self.buf = StringIO()
+        self.level = level
+        self.verbose = verbose
+        self.filename = filename
+        self.WARN_LEVEL = 2
+        self.NOTICE_LEVEL = 4
+        self.ERROR_LEVEL = 0
+        self.OUTPUT_LEVEL = 0
+    def __str__(self):
+        return self.buf.getvalue()
+
+    def log(self, log_level, str):
+        if(self.level > log_level):
+            self.write(str)
+
+    def warn(self, str):
+        self.log(self.WARN_LEVEL, str)
+
+    def notice(self, str):
+        self.log(self.NOTICE_LEVEL, str)
+
+    def error(self, str):
+        self.log(self.ERROR_LEVEL, str)
+
+    def output(self, str):
+        self.log(self.OUTPUT_LEVEL, str)
+
+    def write(self, str):
+        self.buf.write("[%s] %s\n" % (datetime.now(), str))
+        if self.verbose:
+            print str
+        
+    def to_file(self):
+        f = open(self.filename, "w+")
+        f.write(str(self))
+        f.close()
+
 def getResponse(url, method, digits):
     data = None
 
     if method == 'POST' and digits:
-        data = "Digits=%s" % digits
+        data = urllib.urlencode( {'Digits' :  digits} )
     elif method == 'GET' and digits:
         purl = urlparse.urlparse(url)
-        url = "%s?%s&Digits=%s" % \
-            ( purl.geturl(), purl.query , digits)
+        url = "%s?%s&%s" % \
+            ( purl.geturl(), 
+              purl.query , 
+              urllib.urlencode(
+                {'Digits': digits}
+                )
+              )
         
-    print '[%s] %s \n%s' % (method, url, data)
+    logger.notice('[%s] %s \n%s' % (method, url, data))
+
     try:
         req = urllib2.Request(url, data)
         fd = urllib2.urlopen(req)
     except IOError, e:
         print '[Not Found]', e
-        sys.exit(1)
+        exit_handler()
 
     return fd.read()
 
 def end_pause():
-    print "[End Pause]"
+    logger.notice("[End Pause]")
     return None
 
 def input_timeout(*args):
-    print "[Gather timed out]"
-
+    print
+    logger.notice("[Gather timed out]")
+    
 def exit_handler(*args):
+    logger.to_file()
     sys.stdout.flush()
     sys.exit(0)
-
 
 def timed_input(prompt, timeout):
     signal.alarm(timeout)
@@ -110,27 +161,27 @@ def Gather(node):
 
 def Say(node):
     if not node.hasChildNodes():
-        print "text node for <Say> not found."
-        sys.exit(1)
+        logger.error("text node for <Say> not found.")
+        exit_handler()
 
     if len(node.childNodes) != 1:
-        print "Multiple child nodes for say illegal"
-        sys.exit(1)
+        logger.error("Multiple child nodes for say illegal")
+        exit_handler()
 
-    print "[Say] %s" % node.childNodes[0].data.strip()
+    logger.output("[Say] %s" % node.childNodes[0].data.strip())
 
     return None
 
 def Play(node):
     if not node.hasChildNodes():
-        print "text node for <Play> not found."
-        sys.exit(1)
+        logger.error("text node for <Play> not found.")
+        exit_handler()
 
     if len(node.childNodes) != 1:
-        print "Multiple child nodes for play illegal"
-        sys.exit(1)
+        logger.error("Multiple child nodes for play illegal")
+        exit_handler()
     
-    print "[Play] %s" % node.childNodes[0].data
+    logger.output("[Play] %s" % node.childNodes[0].data)
 
     return None
 
@@ -140,41 +191,41 @@ def Pause(node):
             node.attributes.has_key('length'):
         length = node.attributes['length'].value
     
-    print "[Pause length=%s]" % length
+    logger.output("[Pause length=%s]" % length)
     t = Timer(float(length), end_pause)
     t.start()
     return None
 
 def Dial(node):
     if not node.hasChildNodes():
-        print "text node for <Play> not found."
-        sys.exit(1)
+        logger.error("text node for <Play> not found.")
+        exit_handler()
 
     if len(node.childNodes) == 1 and \
             node.childNodes[0].nodeType == node.TEXT_NODE:
-        print "[Dial] %s" % node.childNodes[0].data.strip()
+        logger.notice("[Dial] %s" % node.childNodes[0].data.strip())
         return None
 
-    print "[Dial with children]"
+    logger.notice("[Dial with children]")
     [processNode(child) for child in node.childNodes]
 
     return None
 
 def Number(node):
     if not node.hasChildNodes():
-        print "text node for <Number> not found."
-        sys.exit(1)
+        logger.error("text node for <Number> not found.")
+        exit_handler()
 
-    print "[Number] %s" % node.childNodes[0].data.encode('ascii').strip()
+    logger.output("[Number] %s" % node.childNodes[0].data.encode('ascii').strip())
 
     return None
 
 def Redirect(node):
     if len(node.childNodes) != 1:
-        print "Redirect Syntax Error"
-        sys.exit(1)
+        logger.error("Redirect Syntax Error")
+        exit_handler()
 
-    print "[Redirect] ", node.childNodes[0].data
+    logger.notice("[Redirect] %s" % node.childNodes[0].data)
 
     request = { 
         'action' : node.childNodes[0].data, 
@@ -220,46 +271,43 @@ def Record(node):
     return request
 
 def Hangup(node):
-    print "[Hangup]"
-    sys.exit(0)
-
+    logger.output("[Hangup]")
+    exit_handler()
+    
 def processNode(node):
     action = node.nodeName.encode('ascii')
     if node.nodeType == node.TEXT_NODE:
-        print node.data.strip()
+        logger.output(node.data.strip())
     else:
         return eval("%s(node)" % action)
 
     return None
 
 def emulate(url, method = 'GET', digits = None):
-    print '[Emulation Start] %s' % url
+    logger.notice('[Emulation Start] %s' % url)
     response = getResponse(
         url,
         method,
         digits)
     if not response:
-        print '[Emulation Failed to start]'
-        sys.exit(1)
+        logger.error('[Emulation Failed to start]')
+        exit_handler()
     
     try:
         rdoc = parseString(response)
     except ExpatError, e:
         raise TwiMLSyntaxError(e.lineno, e.offset, response)
 
-    # finally:
-        # exit_handler()
-
     try:
         respNode = rdoc.getElementsByTagName('Response')[0]
     except IndexError, e:
-        print '[No response node] exiting'
-        sys.exit(1)
+        logger.error('[No response node] exiting')
+        exit_handler()
     
     if not respNode.hasChildNodes():
         #hangup
-        print 'Hanging up'
-        sys.exit(0)
+        logger.notice('Hanging up')
+        exit_handler()
         
     nodes = respNode.childNodes
     for node in nodes:
@@ -277,25 +325,41 @@ def emulate(url, method = 'GET', digits = None):
                         request['method'], 
                         request['digits'])
             except TwiMLSyntaxError, e:
-                print e
+                logger.error(e)
                 exit_handler()
 
 def main():
     signal.signal(signal.SIGALRM, input_timeout)
     signal.signal(signal.SIGINT, exit_handler)
-
     
-    if len(sys.argv) > 1:
-        try:
-            emulate(sys.argv[1])
-        except TwiMLSyntaxError, e:
-            print e
-            exit_handler()
-        print '[Phone call ended]'
-        
-    else:
-        print "twilio-emulator.py [url]"
+    usage = "usage: %prog [options] [url]"
+    parser = OptionParser(usage)
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose",
+                      help="Display notices")
+    parser.add_option("-o", "--log-output",
+                      action="store", dest="filename",
+                      help="Where to store log of phone call")
+    (options, args) = parser.parse_args(sys.argv)
 
+    if len(args) != 2:
+        parser.print_help()
+        exit_handler()
 
+    if options.verbose:
+        logger.level = 5
+
+    if options.filename:
+        logger.filename = options.filename
+
+    url = args[1]
+    try:
+        emulate(url)
+    except TwiMLSyntaxError, e:
+        logger.error(e)
+        exit_handler()
+    logger.notice('[Phone call ended]')
+
+logger = ResponseLogger(1, True)
 if __name__ == "__main__":
     main()
